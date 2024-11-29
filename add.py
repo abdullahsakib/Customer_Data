e28fec0645fb697492befef46f97ae6166f4df36
abdullahsakib <abdullahsakib33@gmail.com>
2024-11-29 23:45:55 +0600

    new

--- add.py
+++ add.py
@@ -1,6 +1,11 @@
 
 
 
+name=str(input("Enter Person Name"))
+email=str(input("Enter Person E-Mail adress"))
+phone=int(input("Enter Person Mobile Number"))
+adress=str(input("Enter Person contact adress"))
+
 dic={
     "Name":name,
     "Email":email,
