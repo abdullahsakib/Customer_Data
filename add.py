de2824bdd99464d8de07de9ee7be2beeaa705fd5
abdullahsakib <abdullahsakib33@gmail.com>
2024-11-30 19:10:08 +0600

    new

--- add.py
+++ add.py
@@ -1,20 +1,25 @@
 
 
 
-name=str(input("Enter Person Name"))
-email=str(input("Enter Person E-Mail adress"))
-phone=int(input("Enter Person Mobile Number"))
-adress=str(input("Enter Person contact adress"))
+def add_contact():
+    name=str(input("Enter Person Name"))
+    email=str(input("Enter Person E-Mail adress"))
+    phone=int(input("Enter Person Mobile Number"))
+    adress=str(input("Enter Person contact adress"))
+    dic={}
 
-dic={
-    "Name":name,
-    "Email":email,
-    "phone":phone,
-    "adress":adress
-}
+    # dic={
+    #     "Name":name,
+    #     "Email":email,
+    #     "phone":phone,
+    #     "adress":adress}
+    dic["Name"]=name
+    dic["Email"]=email
+    dic["phone"]=phone
+    dic['adress']=adress
+
+    print(dic)
 
 
 
-def add():
-    pass
-    
+
---
+++ main.py
@@ -0,0 +1,32 @@
+from add import add_contact
+
+all_contacts=[]
+
+while True:
+    print("Welcome Contact Book Management System")
+    print("0 -> if you wanna exit")
+    print("1 -> to add Contact")
+    print("2 -> to view Contact")
+    print("3 -> to update value")
+    print("4 -> to delete Contact")
+
+    menu=input('Select any number from 0 to 4')
+
+    if menu=="0":
+        print("Thanks for using Contact Book Management System ")
+        break
+    elif menu=="1":
+        pass
+        all_books=add_contact(all_books)
+    # elif menu=="2":
+    #     view_all_books(all_books)
+    # elif menu=="3":
+    #     line=int(input("enter line no"))
+    #     update(all_books,line)
+
+    # elif menu=="4":
+    #     line = int(input("Enter the line number of the book you want to delete"))
+    #     delete(all_books,line)
+
+    else:
+        print('select a valid number')
