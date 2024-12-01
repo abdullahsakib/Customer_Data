a276698e0cc17a1a5e16a9556b2ebebee550b2f9
abdullahsakib <abdullahsakib33@gmail.com>
2024-12-01 21:30:04 +0600

    new

--- add.py
+++ add.py
@@ -2,32 +2,37 @@
 from save import save_all
 
 def add_contact(all_contacts):
-    name=str(input("Enter Person Name  :").strip())
-    email=str(input("Enter Person E-Mail address  :").strip())
-    phone=int(input("Enter Person Mobile Number  :").strip())
-    if not phone.isdigit():
-        print("phone number must be integer")
-        return all_contacts
-
-    address=str(input("Enter Person contact address   :").strip())
-
-    for contact in all_contacts:
-        if contact['phone']==phone:
-            print("can't accept duplicate phn number")
+    try:
+        name=str(input("Enter Person Name  :").strip())
+        email=str(input("Enter Person E-Mail address  :").strip())
+        phone=input("Enter Person Mobile Number  :")
+        if not phone.isdigit():
+            print("phone number must be integer")
             return all_contacts
 
-    # take input and directly stores to dictionary
-    dic={
-        "Name":name,
-        "Email":email,
-        "phone":phone,
-        "address":address}
-    all_contacts.append(dic)
-    save_all(all_contacts)
+        address=str(input("Enter Person contact address   :").strip())
 
-    print('contacts added succesfully')
+        for contact in all_contacts:
+            if contact['phone']==phone:
+                print("can't accept duplicate phn number")
+                return all_contacts
 
-    return all_contacts
+        # take input and directly stores to dictionary
+        dic={
+            "Name":name,
+            "Email":email,
+            "phone":phone,
+            "address":address}
+        all_contacts.append(dic)
+        save_all(all_contacts)
+
+        print('contacts added succesfully')
+
+        return all_contacts
+    
+    except Exception as e:
+        print("Error",e)
+
 
  
 
